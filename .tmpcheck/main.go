e59fb396cd5d2e0f66548746761f2c0f23676b51
edisonshen <edisonshen87@gmail.com>
2026-04-28 00:07:34 -0700

    chore: remove stray .tmpcheck/main.go debug script
    Accidentally staged in iter-8 commit; was a one-off probe for
    SafeLockComponent path output. Not part of the production tree.

--- .tmpcheck/main.go
+++
@@ -1,6 +0,0 @@
-package main
-import (
-  "fmt"
-  "github.com/edisonshen/fleet/internal/state"
-)
-func main(){ fmt.Printf("%q\n", state.SafeLockComponent(".")); fmt.Printf("%q\n", state.SafeLockComponent("..")) }
